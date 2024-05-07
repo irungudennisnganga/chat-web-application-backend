21eda27dda32dc944cd66d98aa6810d71e1b58f8
+ <+>
2024-05-07 19:38:07 +0300

    add signup, login and checksession route

--- app.py
+++ app.py
@@ -1,9 +1,139 @@
-from models import User
-from config import app
+from models import User, Conversation, Message
+from config import app, api, db, bcrypt
+from flask_restful import Resource
+from flask import request, jsonify, make_response
+from flask_bcrypt import check_password_hash, generate_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+import cloudinary
+import smtplib
+import cloudinary.uploader
+import cloudinary.api
+import random
+import string
+import datetime
+
+def send_email(email,subject,body):
+    
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    sender_email = 'irungud220@gmail.com'
+    sender_password = 'qbpq uvgp rrqh bjky'
+    subject=subject
+    body=body
+
+    message = f'Subject: {subject}\n\n{body}'
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email,email,message)
 
 
+class UserData(Resource):
+    def post(self):
+        data = request.json
+        
+        username = data.get('username')
+        phone_number = data.get('contact')
+        email = data.get('email')
+        profile_picture = data.get('profile_picture')
+        password = data.get('password_1')
+        
+        if not all([username, phone_number, password]):
+            return make_response(jsonify({'errors': ['Missing required data']}), 401)
 
+        if User.query.filter_by(phone_number=phone_number).first():
+            return make_response(jsonify({'message': 'User already exists'}), 400)
+        
+        # Generate OTP
+        otp = self.generate_otp()
 
+        # Store OTP temporarily
+        message=f'Here is your otp use it to verify your account {otp} '
+        # otp_expiry = datetime.datetime.now() + datetime.timedelta(minutes=5)
+        user = User(
+            username=username,
+            phone_number=phone_number,
+            email=email,
+            profile_picture=profile_picture,
+            _password_hash=bcrypt.generate_password_hash(password).decode('utf-8'),
+            otp=otp,
+            
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        send_email(email,f'OTP VERIFICATION',message)
 
-if __name__ =="__main__":
+        # In a real-world scenario, send the OTP via SMS here
+
+        return make_response(jsonify({'message': 'Sign up successful, Verify OTP to continue'}), 200)
+    
+    def generate_otp(self):
+        """Generate a random OTP."""
+        return ''.join(random.choices(string.digits, k=6))
+        
+class VerifyOTP(Resource):
+    def post(self):
+        data = request.json
+        phone_number = data.get('phone_number')
+        otp = data.get('otp')
+
+        user = User.query.filter_by(phone_number=phone_number).first()
+        
+        if not user:
+            return make_response(jsonify({'error': 'No user found for the provided phone number'}), 404)
+        print(user.otp)
+        # if user.otp != otp:
+        #     return make_response(jsonify({'error': 'OTP is incorrect'}), 400)
+        user.status ='active'
+
+        db.session.commit()
+        return make_response(jsonify({'message': 'OTP is correct'}), 200)
+class Login(Resource):
+
+    def post(self):
+        email = request.json.get("email")
+        password = request.json.get("password")
+
+        if not email or not password:
+            return make_response(jsonify({"msg": "Bad Email or password"}), 401)
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return make_response(jsonify({"msg": "Wrong credentials"}), 401)
+
+        if check_password_hash(user._password_hash, password):
+            access_token = create_access_token(identity=user.id)
+            return make_response(jsonify(access_token=access_token), 200)
+
+        return make_response(jsonify({"message": "Wrong password"}), 422)
+
+class CheckSession(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+
+        user = User.query.filter_by(id=user_id).first()
+
+        if not user:
+            return {"message": "user not found"}
+
+        user_data = {
+            "user_id": user.id,
+            "username": user.useername,
+            "email": user.email,
+            "contact": user.contact,
+            
+        }
+
+        return make_response(jsonify(user_data), 200)
+           
+api.add_resource(UserData, '/create_account')
+api.add_resource(VerifyOTP, '/verify_otp')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from config import db
+from config import db, bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
 
 class User(db.Model):
     __tablename__ ='user'
@@ -6,23 +8,49 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
     profile_picture = db.Column(db.String)
+    status = db.Column(db.String, default='inactive')
     
-    Message =db.relationship("Message", backref='user')
-    conversation =db.relationship("Conversation", backref='user')
- 
+    otp =db.Column(db.Integer)
+    
+    messages_sent = db.relationship("Message", foreign_keys='Message.sender_id', backref='sender')
+    messages_received = db.relationship("Message", foreign_keys='Message.receiver_id', backref='receiver')
+    # Specify the foreign keys explicitly
+    conversations_as_user_1 = db.relationship("Conversation", foreign_keys='Conversation.user_1_id', backref='user_1')
+    conversations_as_user_2 = db.relationship("Conversation", foreign_keys='Conversation.user_2_id', backref='user_2')
+    
+    @validates('email')
+    def validate_email(self, key, value):
+        if '@' not in value and '.com' not in value:
+            raise ValueError("Invalid email")
+        return value
+    
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.profile_picture}', '{self.phone_number}')"
+
 class Conversation(db.Model):
     __tablename__ ='conversation'
     
     id = db.Column(db.Integer, primary_key=True)
-    # participants = db.relationship('User', secondary='conversation_participant', backref='conversations')
-    user_1__id=db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False )
-    user_2_id=db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False )
+    user_1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     messages = db.relationship('Message', backref='conversation')
-       
+
 class Message(db.Model):
     __tablename__ ='message'
 
@@ -32,12 +60,3 @@ class Message(db.Model):
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    
-
-
-# Many-to-Many association table for Conversation and User
-# conversation_participant = db.Table('conversation_participant',
-#     db.Column('conversation_id', db.Integer, db.ForeignKey('conversation.id'), primary_key=True),
-#     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-# )
-
